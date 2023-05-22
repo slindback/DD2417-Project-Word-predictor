e24c610337de50c8077059e688fe0f30cfe0ae65
Simon Lindbäck <slindbac@kth.se>
2023-05-22 13:28:08 +0200

    Not used anymore.

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 # Written by Simon Lindbäck and Anton Lewander
+"""
 from BigramTrainer import BigramTrainer
+from Predictor import Predictor
 
 import argparse
 import operator
 import os
-"""
+
 def clean_line(line):
     clean_str, clean_line = "", []
     line = str(line).lower()
@@ -19,13 +21,13 @@ def read_json(path):
         data = json.load(file)
     for i in range(len(messages)):
         yield clean_line(messages[i]['text']['$']):
-"""
+
 
 def main():
     """
-    Parse command line arguments
+    #Parse command line arguments
     """
-    """
+
     parser = argparse.ArgumentParser(description='BigramTrainer')
     parser.add_argument('--file', '-f', type=str,  required=True, help='file from which to build the language model')
     parser.add_argument('--destination', '-d', type=str, help='file in which to store the language model')
@@ -42,16 +44,38 @@ def main():
     else:
         for row in stats: print(row)
     """
+    """
     current_dir = os.getcwd()
-    json_path = os.path.join(current_dir,'data', 'smsCorpus_en_2015.03.09_all.json')
+    json_path = os.path.join(current_dir,'data', 'smsCorpus_en.json')
+
     bigram_trainer = BigramTrainer(json_path)
     bigram_trainer.process_file() # Calculating unigram and bigram counts
     stats = bigram_trainer.stats() # Get bigram stats
-    stats_path = os.path.join('data', 'stats.txt')
-    if not os.path.exists(stats_path):
+
+    model_path = os.path.join('data', 'smsCorpus_en_model.txt')
+
+    if not os.path.exists(model_path):
         os.makedirs('data', exist_ok=True)
-        with open(stats_path,'w',encoding='utf-8') as file:
+        with open(model_path,'w',encoding='utf-8') as file:
             for row in stats: file.write(row + '\n')
+
+    bigram_trainer = BigramTrainer()
+
+    model2path = os.path.join('data', 'Purdue_Calumet_text_Message_Corpus_model.txt')
+    if not os.path.exists(file_path):
+        bigram_trainer.process_xml()
+
+
+
+
+
+    predictor = Predictor(model_path)
+    predictor.read_model()
+    inp = ''
+    while inp != "!":
+        inp = input()
+        predictions = predictor.predict_next_word(inp.lower())
+        print(predictions)
 
     #unigram_count = {}
     #for sentence in read_json(json_path):
@@ -63,3 +87,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
