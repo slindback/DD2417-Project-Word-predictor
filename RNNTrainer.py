f392ce728bbd9c03742ec7ed0163daaeb6072538
Simon Lindbäck <slindbac@kth.se>
2023-06-08 10:39:42 +0200

    Bugs fixed

--- RNNTrainer.py
+++ RNNTrainer.py
@@ -20,20 +20,22 @@ def main():
     current_time = now.strftime("%H:%M:%S")
     print("Starting at ", current_time)
 
-    strokes_saved = 0
-    total_chars = 0
-
-    for sentence in sentences:
-        sentence = sentence.split()
-        total_chars += sum(len(word) for word in sentence)
-        for i in range(1,len(sentence)):
-            if len(sentence) > 1:
-                predicted_words = rnn_main(sentence[:i],arguments.file,arguments.destination,arguments.epochs)
+    # s: number of words to send to the RNN predictor. Range 1 to 16
+    # (16 is the length of the longest sentence in the test set)
+    for s in range(1,17):
+        strokes_saved = 0
+        total_chars = 0
+        for sentence in sentences:
+            sentence = sentence.split()
+            total_chars += sum(len(word) for word in sentence)
+            for i in range(1,len(sentence)):
+                if i-s > -1: predicted_words = rnn_main(sentence[i-s:i],arguments.file,arguments.destination,arguments.epochs)
+                else:        predicted_words = rnn_main(sentence[:i],arguments.file,arguments.destination,arguments.epochs)
                 if sentence[i] in predicted_words:
                     strokes_saved += len(sentence[i])
 
-    percentage = str(round((strokes_saved / total_chars) * 100, 2))
-    print("{} keystrokes saved with {}. Percentage: {}%".format(strokes_saved, arguments.destination, percentage))
+        percentage = str(round((strokes_saved / total_chars) * 100, 2))
+        print("{} keystrokes saved with {}. {} words sent to predictor. Percentage: {}%".format(strokes_saved, arguments.destination, s, percentage))
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
